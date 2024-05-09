9108f2b9b8f4a0a892f601f76d34c7369e436dd4
DESKTOP-5Q2RE7E\roger <rlgh135@gmail.com>
2024-05-09 17:32:15 +0900

    박기호: 쿼리문 통합, 수정

--- src/main/resources/tripfy.sql
+++ src/main/resources/tripfy.sql
@@ -1,3 +1,7 @@
+create database tripfy;
+
+use tripfy;
+
 CREATE TABLE `user` (
   `userid` varchar(300) PRIMARY KEY,
   `userpw` varchar(300),
@@ -9,32 +13,44 @@ CREATE TABLE `user` (
   `addrdetail` varchar(300),
   `addretc` varchar(300),
   `regdate` date DEFAULT 'now()',
-  `u_warncnt` bigint AUTO_INCREMENT DEFAULT 0,
+  `user_warncnt` bigint AUTO_INCREMENT DEFAULT 0,
   `isDelete` int DEFAULT 0
 );
 
-CREATE TABLE `u_img` (
-  `userid` varcahr(300),
-  `systemname` varchar(2000),
+CREATE TABLE `user_img` (
+  `userid` varchar(300),
+  `sysname` varchar(2000),
   `orgname` varchar(2000)
 );
 
 CREATE TABLE `manager` (
   `managerid` varchar(300) PRIMARY KEY,
-  `managerpw` varcahr(300)
+  `managerpw` varchar(300)
 );
 
 CREATE TABLE `guide` (
   `guidenum` bigint PRIMARY KEY AUTO_INCREMENT,
   `userid` varchar(300),
-  `g_likecnt` bigint AUTO_INCREMENT DEFAULT 0,
-  `g_warncnt` bigint AUTO_INCREMENT DEFAULT 0
+  `guide_likecnt` bigint AUTO_INCREMENT DEFAULT 0,
+  `guide_warncnt` bigint AUTO_INCREMENT DEFAULT 0
+);
+
+create table `guide_user`(
+	`guidenum` bigint,
+    `userid` varchar(300)
+);
+
+create table `review` (
+	`guidenum` bigint,
+    `userid` varchar(300),
+    `contents` text,
+    `packagenum` bigint
 );
 
 CREATE TABLE `region` (
   `regionname` varchar(300),
-  `contrycode` varchar(300),
-  PRIMARY KEY (`regionname`, `contrycode`)
+  `countrycode` varchar(300),
+  PRIMARY KEY (`regionname`, `countrycode`)
 );
 
 CREATE TABLE `board` (
@@ -43,63 +59,63 @@ CREATE TABLE `board` (
   `title` varchar(300),
   `content` text,
   `regdate` datetime DEFAULT 'now()',
-  `updateck` int DEFAULT 0,
+  `updatecheck` int DEFAULT 0,
   `regionname` varchar(300),
-  `contrycode` varchar(300),
+  `countrycode` varchar(300),
   `likecnt` bigint,
   `viewcnt` bigint
 );
 
-CREATE TABLE `boardadd` (
+CREATE TABLE `boardaddr` (
   `boarnum` bigint,
-  `place_name` varchar(300),
-  `road_address` varchar(300),
+  `placename` varchar(300),
+  `roadaddress` varchar(300),
   `address` varchar(300),
   `duedate` date,
   `enddate` date
 );
 
-CREATE TABLE `b_reply` (
+CREATE TABLE `board_reply` (
   `replynum` bigint PRIMARY KEY AUTO_INCREMENT,
   `boardnum` bigint,
   `userid` varchar(300),
-  `updateck` int DEFAULT 0,
-  `em_systemname` varchar(300),
+  `updatecheck` int DEFAULT 0,
+  `em_sysname` varchar(300),
   `contents` varchar(2000),
   `regdate` datetime DEFAULT 'now()'
 );
 
-CREATE TABLE `b_file` (
+CREATE TABLE `board_file` (
   `boardnum` bigint,
-  `systemname` varchar(300) PRIMARY KEY,
+  `sysname` varchar(300) PRIMARY KEY,
   `orgname` varchar(300)
 );
 
-CREATE TABLE `packages` (
+CREATE TABLE `package` (
   `pacakgenum` BIGINT PRIMARY KEY AUTO_INCREMENT,
   `guidenum` bigint,
-  `p_title` varchar(300),
-  `p_content` text,
+  `package_title` varchar(300),
+  `package_content` text,
   `maxcnt` int,
-  `a_price` int,
-  `c_price` int,
-  `p_startdate` DATETIME,
-  `p_enddate` VARCHAR(300),
-  `p_tourdays` int,
+  `adult_price` int,
+  `child_price` int,
+  `startdate` DATETIME,
+  `enddate` VARCHAR(300),
+  `tourdays` int,
   `viewcnt` bigint,
   `deadline` date NOT NULL,
-  `isDelete` int DEFAULT 0
+  `isdelete` int DEFAULT 0
 );
 
 CREATE TABLE `timeline` (
   `timelinenum` bigint PRIMARY KEY AUTO_INCREMENT,
   `packagenum` bigint,
   `day` int,
-  `tit` text,
-  `cont` text
+  `title` text,
+  `contents` text
 );
 
-CREATE TABLE `packages_file` (
+CREATE TABLE `package_file` (
   `packagenum` bigint,
   `pf_sysname` VARCHAR(300) PRIMARY KEY,
   `pf_orgname` VARCHAR(300)
@@ -112,15 +128,15 @@ CREATE TABLE `timeline_file` (
 );
 
 CREATE TABLE `reservation` (
-  `resernum` bigint PRIMARY KEY AUTO_INCREMENT,
+  `reservationnum` bigint PRIMARY KEY AUTO_INCREMENT,
   `packagenum` bigint,
-  `a_cnt` int,
-  `c_cnt` int,
+  `adult_cnt` int,
+  `child_cnt` int,
   `userid` varchar(300),
   `keycode` varchar(300),
   `price` varchar(300),
-  `p_method` varchar(300),
-  `is_delete` int DEFAULT 0
+  `pay_method` varchar(300),
+  `isdelete` int DEFAULT 0
 );
 
 CREATE TABLE `not_user` (
@@ -130,7 +146,7 @@ CREATE TABLE `not_user` (
   `keycode` varchar(300) PRIMARY KEY
 );
 
-CREATE TABLE `p_report` (
+CREATE TABLE `report` (
   `userid` varchar(300),
   `reporturl` varchar(300),
   `reportcontents` text,
@@ -138,15 +154,15 @@ CREATE TABLE `p_report` (
 );
 
 CREATE TABLE `chat` (
-  `chat_id` bigint PRIMARY KEY AUTO_INCREMENT,
-  `userid_1` varchar(300),
-  `userid_2` varchar(300),
+  `chatid` bigint PRIMARY KEY AUTO_INCREMENT,
+  `userid_a` varchar(300),
+  `userid_b` varchar(300),
   `packagenum` bigint
 );
 
 CREATE TABLE `chat_detail` (
   `messagenum` bigint PRIMARY KEY AUTO_INCREMENT,
-  `chat_id` bigint,
+  `chatid` bigint,
   `senderid` varchar(300),
   `message` text,
   `regdate` datetime DEFAULT 'now()'
