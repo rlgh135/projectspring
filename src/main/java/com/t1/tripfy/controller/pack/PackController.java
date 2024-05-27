289654e58e3bcdbcdfa1132af16d746d228556a5
Burgundysocks <ia05125@naver.com>
2024-05-27 14:50:15 +0900

    진짜 추가 찐막이길 바랍니다 정상현

--- src/main/java/com/t1/tripfy/controller/pack/PackController.java
+++ src/main/java/com/t1/tripfy/controller/pack/PackController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -175,6 +176,12 @@ public class PackController {
 		
 		return "package/pay";
 	}
+	//추가
+		@GetMapping("thumbnail")
+		public ResponseEntity<Resource> thumbnail(String systemname) throws Exception {
+			System.out.println(systemname);
+			return service.getThumbnailResource(systemname);
+		}
 	
 	@PostMapping("write")
 	public String write(PackageDTO pack, MultipartFile packageFile,HttpServletRequest req) throws Exception {
