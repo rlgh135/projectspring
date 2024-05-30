242f6b684f6cee2f46422abc70b6ed14fa089787
DESKTOP-EHG3VFT\user1 <yeonghwna990@gmail.com>
2024-05-30 15:22:12 +0900

    최영환 에러페이지 간단하게 만들어 놨습니다

---
+++ src/main/java/com/t1/tripfy/controller/error/ErrorController.java
@@ -0,0 +1,20 @@
+package com.t1.tripfy.controller.error;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController {
+	@GetMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Integer statusCode = (Integer) request.getAttribute("javax.servlet.error.status_code");
+        if (statusCode == 404) {
+            return "error/404";
+        } else if (statusCode == 500) {
+            return "error/500";
+        }
+        return "error/error";
+    }
+}
---
+++ src/main/resources/static/js/error/error.js
@@ -0,0 +1,4 @@
+const home_btn = document.querySelector("#home_btn");
+home_btn.addEventListener("click", () => {
+    window.location.href = "/";
+});
