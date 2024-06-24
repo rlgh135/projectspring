c37e0ae263a45660dbabbac7210152e20e50a60c
Burgundysocks <136434305+Burgundysocks@users.noreply.github.com>
2024-06-24 16:41:09 +0900

    Update tripfy.sql

--- src/main/resources/tripfy.sql
+++ src/main/resources/tripfy.sql
@@ -1,5 +1,6 @@
-create database tripfy;
-use tripfy;
+CREATE DATABASE tripfy;
+drop database tripfy;
+USE tripfy;
 
 CREATE TABLE `user` (
   `userid` varchar(300) PRIMARY KEY,
@@ -10,10 +11,6 @@ CREATE TABLE `user` (
   `birth` varchar(300),
   `addr` varchar(300),
   `placeid` varchar(300),
-  /*
-  date 디폴트 now()에서 수정
-  워닝카운트에 auto increment 달려있는거 지움
-  */
   `regdate` date DEFAULT (current_date),
   `user_warncnt` bigint DEFAULT 0,
   `isDelete` int DEFAULT 0,
@@ -23,7 +20,8 @@ CREATE TABLE `user` (
 CREATE TABLE `user_img` (
   `userid` varchar(300),
   `sysname` varchar(2000),
-  `orgname` varchar(2000)
+  `orgname` varchar(2000),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `guide` (
@@ -31,20 +29,26 @@ CREATE TABLE `guide` (
   `userid` varchar(300),
   `guide_likecnt` bigint DEFAULT 0,
   `guide_warncnt` bigint DEFAULT 0,
-  `introduce` text
+  `introduce` text,
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
-create table `guide_user`(
-	`guidenum` bigint,
-    `userid` varchar(300)
+CREATE TABLE `guide_user`(
+  `guidenum` bigint,
+  `userid` varchar(300),
+  FOREIGN KEY (`guidenum`) REFERENCES `guide`(`guidenum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
-create table `review` (
-	`guidenum` bigint,
-    `userid` varchar(300),
-    `contents` text,
-    `packagenum` bigint,
-    `em_sysname` varchar(300)
+CREATE TABLE `review` (
+  `guidenum` bigint,
+  `userid` varchar(300),
+  `contents` text,
+  `packagenum` bigint,
+  `em_sysname` varchar(300),
+  FOREIGN KEY (`guidenum`) REFERENCES `guide`(`guidenum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`),
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
 );
 
 CREATE TABLE `region` (
@@ -53,14 +57,14 @@ CREATE TABLE `region` (
   PRIMARY KEY (`regionname`, `countrycode`)
 );
 
-insert into region values('kr','서울');
-insert into region values('kr','제주도');
-insert into region values('kr','경기도');
-insert into region values('kr','강원도');
-insert into region values('kr','충청도');
-insert into region values('kr','경상도');
-insert into region values('kr','전라도');
-insert into region values('kr','인천광역시');
+INSERT INTO region VALUES ('kr','서울');
+INSERT INTO region VALUES ('kr','제주도');
+INSERT INTO region VALUES ('kr','경기도');
+INSERT INTO region VALUES ('kr','강원도');
+INSERT INTO region VALUES ('kr','충청도');
+INSERT INTO region VALUES ('kr','경상도');
+INSERT INTO region VALUES ('kr','전라도');
+INSERT INTO region VALUES ('kr','인천광역시');
 
 CREATE TABLE `board` (
   `boardnum` bigint PRIMARY KEY AUTO_INCREMENT,
@@ -71,21 +75,26 @@ CREATE TABLE `board` (
   `updatecheck` int DEFAULT 0,
   `regionname` varchar(300),
   `countrycode` varchar(300),
-  `likecnt` bigint default 0,
-  `viewcnt` bigint default 0,
-  `replycnt` bigint default 0
+  `likecnt` bigint DEFAULT 0,
+  `viewcnt` bigint DEFAULT 0,
+  `replycnt` bigint DEFAULT 0,
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`),
+  FOREIGN KEY (`regionname`, `countrycode`) REFERENCES `region`(`regionname`, `countrycode`)
 );
 
-create table board_like (
-	`boardnum` bigint,
-    `userid` varchar(300)
+CREATE TABLE `board_like` (
+  `boardnum` bigint,
+  `userid` varchar(300),
+  FOREIGN KEY (`boardnum`) REFERENCES `board`(`boardnum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `boardaddr` (
   `boardnum` bigint,
   `placename` varchar(300),
   `startdate` date,
-  `enddate` date
+  `enddate` date,
+  FOREIGN KEY (`boardnum`) REFERENCES `board`(`boardnum`)
 );
 
 CREATE TABLE `board_reply` (
@@ -95,37 +104,44 @@ CREATE TABLE `board_reply` (
   `updatecheck` int DEFAULT 0,
   `em_sysname` varchar(300),
   `contents` varchar(2000),
-  `regdate` datetime DEFAULT now()
+  `regdate` datetime DEFAULT now(),
+  FOREIGN KEY (`boardnum`) REFERENCES `board`(`boardnum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `board_file` (
   `boardnum` bigint,
   `sysname` varchar(300),
-  `orgname` varchar(300)
+  `orgname` varchar(300),
+  FOREIGN KEY (`boardnum`) REFERENCES `board`(`boardnum`)
 );
 
 CREATE TABLE `package` (
-  `packagenum` BIGINT PRIMARY KEY AUTO_INCREMENT,
+  `packagenum` bigint PRIMARY KEY AUTO_INCREMENT,
   `guidenum` bigint,
   `package_title` varchar(300),
   `package_content` text,
   `maxcnt` int,
   `adult_price` int,
   `child_price` int,
-  `startdate` VARCHAR(300),
-  `enddate` VARCHAR(300),
+  `startdate` varchar(300),
+  `enddate` varchar(300),
   `tourdays` int,
-  `viewcnt` bigint default 0,
+  `viewcnt` bigint DEFAULT 0,
   `deadline` date NOT NULL,
   `isdelete` int DEFAULT 0,
   `regionname` varchar(300),
   `countrycode` varchar(300),
-  /*아래 컬럼이O이면 패키지리스트에 띄우고 x면 안띄울겁니다*/
-  `Visibility` varchar(3) default 'x'
+  `Visibility` varchar(3) DEFAULT 'x',
+  FOREIGN KEY (`guidenum`) REFERENCES `guide`(`guidenum`),
+  FOREIGN KEY (`regionname`, `countrycode`) REFERENCES `region`(`regionname`, `countrycode`)
 );
-create table package_like (
-	`packagenum` bigint,
-    `userid` varchar(300)
+
+CREATE TABLE `package_like` (
+  `packagenum` bigint,
+  `userid` varchar(300),
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `timeline` (
@@ -134,32 +150,24 @@ CREATE TABLE `timeline` (
   `day` int,
   `detail_num` int,
   `title` text,
-  `contents` text
+  `contents` text,
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
 );
 
 CREATE TABLE `package_file` (
   `packagenum` bigint,
-  `pf_sysname` VARCHAR(300),
-  `pf_orgname` VARCHAR(300)
+  `pf_sysname` varchar(300),
+  `pf_orgname` varchar(300),
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
 );
 
 CREATE TABLE `timeline_file` (
   `timelinenum` bigint,
-  `tf_sysname` VARCHAR(300),
-  `tf_orgname` VARCHAR(300)
+  `tf_sysname` varchar(300),
+  `tf_orgname` varchar(300),
+  FOREIGN KEY (`timelinenum`) REFERENCES `timeline`(`timelinenum`)
 );
 
-/*
-isdelete: 0 기본
-isdelete: 1 유저가 취소 신청
-isdelete: 2 가이드도 확인 후 최종 취소
-*/
-/*
-name 추가 및 비회원 name -> '회원' 쿼리
-set SQL_SAFE_UPDATES = 0;
-ALTER TABLE reservation ADD COLUMN name VARCHAR(300);
-UPDATE reservation SET name = '회원' where keycode <> 'user';
-*/
 CREATE TABLE `reservation` (
   `reservationnum` bigint PRIMARY KEY AUTO_INCREMENT,
   `packagenum` bigint,
@@ -172,119 +180,85 @@ CREATE TABLE `reservation` (
   `price` varchar(300),
   `pay_method` varchar(300),
   `isdelete` int DEFAULT 0,
-  `name` varchar(300)
+  `name` varchar(300),
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `not_user` (
   `packagenum` bigint,
   `name` varchar(300),
   `phone` varchar(300),
-  `keycode` varchar(300) PRIMARY KEY
+  `keycode` varchar(300) PRIMARY KEY,
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
 );
 
 CREATE TABLE `report` (
   `userid` varchar(300),
   `reporturl` varchar(300),
   `reportcontents` text,
-  `state` varchar(300) DEFAULT '대기중'
+  `state` varchar(300) DEFAULT '대기중',
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
-/* 240516 채팅테이블 수정
-CREATE TABLE `chat` (
-  `chatid` bigint PRIMARY KEY AUTO_INCREMENT,
-  `userid_a` varchar(300),
-  `userid_b` varchar(300),
-  `packagenum` bigint
+
+# 채팅 테이블
+
+CREATE TABLE `chat_room` (
+  `chat_room_idx` bigint PRIMARY KEY AUTO_INCREMENT,
+  `chat_room_type` int NOT NULL COMMENT "채팅방 타입 - 0 일반/일반, 1 가이드/일반(문의), 2 가이드/일반(다대다)",
+  `chat_room_title` varchar(300) COMMENT "null 가능, 패키지 관련이면 무조건 null",
+  `packagenum` bigint COMMENT "null이면 일반 <-> 일반, null이 아니면 판매자 <-> 일반 <- 정도로 볼 수 있음, 240617부로 chat_room_type 추가, 역할이 넘어감",
+  `chat_room_is_terminated` boolean NOT NULL COMMENT "종료되었는지 여부",
+  `regdate` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP()) COMMENT "채팅방 개설 시간",
+  FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
+);
+
+CREATE TABLE `chat_user` (
+  `chat_room_idx` bigint,
+  `userid` varchar(300),
+  `chat_user_is_creator` boolean NOT NULL COMMENT "채팅방 생성자, 일반 <-> 일반의 경우 중요하지 않음, 판매자 <-> 유저의 경우 판매자가 true",
+  `chat_user_is_quit` boolean NOT NULL COMMENT "해당 사용자가 나갔는지 여부",
+  `chat_detail_idx` bigint COMMENT "각 레코드의 userid가 조회한 제일 최근의 메시지, 순환 참조를 위해 null기입을 가능하게 함, 그리고 자기 자신의 메시지도 기입 가능하게 해야 함",
+  PRIMARY KEY(`chat_room_idx`, `userid`),
+  FOREIGN KEY (`chat_room_idx`) REFERENCES `chat_room`(`chat_room_idx`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
 );
 
 CREATE TABLE `chat_detail` (
+  `chat_detail_idx` bigint PRIMARY KEY AUTO_INCREMENT,
+  `chat_room_idx` bigint NOT NULL,
+  `userid` varchar(300) NOT NULL COMMENT "메시지 작성자",
+  `chat_detail_content` varchar(300) NOT NULL,
+  `regdate` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP()),
+  FOREIGN KEY (`chat_room_idx`) REFERENCES `chat_room`(`chat_room_idx`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
+);
+
+CREATE TABLE `chat_user_regdate` (
+  `cur_idx` bigint PRIMARY KEY AUTO_INCREMENT,
+  `chat_room_idx` bigint NOT NULL,
+  `userid` varchar(300) NOT NULL,
+  `cur_action` char(20) NOT NULL COMMENT "진입/이탈 기록, 최초진입(채팅방 생성시 진입) INIT_ENTER, 일반진입(생성 이후 진입) NORM_ENTER, 이탈 LEAVE",
+  `cur_regdate` datetime NOT NULL DEFAULT (CURRENT_TIMESTAMP()),
+  FOREIGN KEY (`chat_room_idx`) REFERENCES `chat_room`(`chat_room_idx`),
+  FOREIGN KEY (`userid`) REFERENCES `user`(`userid`)
+);
+
+# 매니저와 쪽지
+
+CREATE TABLE `manager`(
+  `manage_key` varchar(1000)
+);
+
+INSERT INTO manager VALUES("X7a5G3kZ9T");
+
+CREATE TABLE `task_message`(
   `messagenum` bigint PRIMARY KEY AUTO_INCREMENT,
-  `chatid` bigint,
-  `senderid` varchar(300),
-  `message` text,
+  `task_type` varchar(300), /*1보드 2패키지 3유저*/
+  `detail_num` varchar(300),
+  `sendid` varchar(300),
+  `contents` text,
+  `answer` text,
+  `task_status` varchar(300) DEFAULT "1", /*1은 진행중 2는 처리완료*/
   `regdate` datetime DEFAULT now()
 );
-*/
-# 240516 채팅테이블 수정본
-#채팅방 목록
-CREATE TABLE `chat_room` (
-	`chat_room_idx` BIGINT PRIMARY KEY AUTO_INCREMENT,
-	`chat_room_type` INT NOT NULL COMMENT "채팅방 타입 - 0 일반/일반, 1 가이드/일반(문의), 2 가이드/일반(다대다)",
-    `chat_room_title` VARCHAR(300) COMMENT "null 가능, 패키지 관련이면 무조건 null",
-    `packagenum` BIGINT COMMENT "null이면 일반 <-> 일반, null이 아니면 판매자 <-> 일반 <- 정도로 볼 수 있음, 240617부로 chat_room_type 추가, 역할이 넘어감",
-    `chat_room_is_terminated` BOOLEAN NOT NULL COMMENT "종료되었는지 여부",
-    `regdate` DATETIME NOT NULL DEFAULT(CURRENT_TIMESTAMP()) COMMENT "채팅방 개설 시간"
-    #,CONSTRAINT fk___package___chat_room___packagenum FOREIGN KEY (`packagenum`) REFERENCES `package`(`packagenum`)
-);
-
-#채팅방 가입자 목록
-CREATE TABLE `chat_user` (
-	`chat_room_idx` BIGINT, 
-    `userid` VARCHAR(300),
-    `chat_user_is_creator` BOOLEAN NOT NULL COMMENT "채팅방 생성자, 일반 <-> 일반의 경우 중요하지 않음, 판매자 <-> 유저의 경우 판매자가 true",
-    `chat_user_is_quit` BOOLEAN NOT NULL COMMENT "해당 사용자가 나갔는지 여부",
-    `chat_detail_idx` BIGINT COMMENT "각 레코드의 userid가 조회한 제일 최근의 메시지, 순환 참조를 위해 null기입을 가능하게 함, 그리고 자기 자신의 메시지도 기입 가능하게 해야 함",
-    PRIMARY KEY(`chat_room_idx`, `userid`)
-    /*
-    ,CONSTRAINT fk___chat_room___chat_user___chat_room_idx FOREIGN KEY (`chat_room_idx`) REFERENCES `chat_room`(`chat_room_idx`),
-    CONSTRAINT fk___user___chat_user___userid FOREIGN KEY (`userid`) REFERENCES `user`(`userid`),
-    CONSTRAINT fk___chat_detail___chat_user___chat_detail_idx FOREIGN KEY (`chat_detail_idx`) REFERENCES `chat_detail`(`chat_detail_idx`)
-    */
-);
-
-#채팅 내역
-CREATE TABLE `chat_detail` (
-	`chat_detail_idx` BIGINT PRIMARY KEY AUTO_INCREMENT,
-    `chat_room_idx` BIGINT NOT NULL,
-    `userid` VARCHAR(300) NOT NULL COMMENT "메시지 작성자",
-    `chat_detail_content` VARCHAR(300) NOT NULL,
-    `regdate` DATETIME NOT NULL DEFAULT(CURRENT_TIMESTAMP())
-    /*
-    ,CONSTRAINT fk___chat_room___chat_detail___chat_room_idx FOREIGN KEY (`chat_room_idx`) REFERENCES `chat_room`(`chat_room_idx`),
-    CONSTRAINT fk___chat_user___chat_detail___userid FOREIGN KEY (`userid`) REFERENCES `chat_user`(`userid`)
-	*/
-);
-
-#유저들의 채팅방 진입/이탈을 기록하기 위한 테이블
-CREATE TABLE `chat_user_regdate` (
-	`cur_idx` BIGINT PRIMARY KEY AUTO_INCREMENT,
-	`chat_room_idx` BIGINT NOT NULL,
-    `userid` VARCHAR(300) NOT NULL,
-    `cur_action` CHAR(20) NOT NULL COMMENT "진입/이탈 기록, 최초진입(채팅방 생성시 진입) INIT_ENTER, 일반진입(생성 이후 진입) NORM_ENTER, 이탈 LEAVE",
-    `cur_regdate` DATETIME NOT NULL DEFAULT(CURRENT_TIMESTAMP())
-);
-
-#매니저와 쪽지
-create table manager(
-	manage_key varchar(1000)
-);
-insert into manager values("X7a5G3kZ9T");
-create table task_message(
-	messagenum bigint primary key auto_increment,
-    task_type varchar(300), /*1보드 2패키지 3유저*/
-    detail_num varchar(300),
-    sendid varchar(300),
-    contents text,
-	answer text,
-    task_status varchar(300) default "1", /*1은 진행중 2는 처리완료*/
-    regdate datetime DEFAULT now()
-);
-select * from chat_room;
-select * from chat_user;
-select * from chat_detail;
-select * from chat_user_regdate;
-drop table chat_room;
-drop table chat_user;
-drop table chat_detail;
-drop table chat_user_regdate;
-drop database tripfy;
-create database tripfy;
-select * from reservation where packagenum=4001;
-select * from board where boardnum=3001;
-select * from board_file where boardnum=3001;
-use tripfy;
-select * from package order by packagenum desc;
-select sum(adult_cnt + child_cnt) from reservation where packagenum =4003 and isdelete < 2;
-select * from timeline;
-update package set visibility='O' where packagenum > 0;
-insert into guide values(1, 'kiho', '0', '0', '자기소개가 없어요');
-insert into guide values(2, 'minsoo', '0', '0', '자기소개가 없어요');
